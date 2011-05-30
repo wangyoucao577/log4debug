147a2d368ace95a7edf1e908d105006559dc9685
wangyoucao577@gmail.com <wangyoucao577@gmail.com@ffcbdcec-20ec-1f73-d8fd-afeda11f1d31>
2011-05-30 16:57:27 +0000

    使用richtextbox替代textbox以实现不同行的不同颜色。 添加一些测试代码。 richTextBox的频繁刷新性能目前还是一个瓶颈。

--- trunk/Log4Debug/Main.cs
+++ trunk/Log4Debug/Main.cs
@@ -5,18 +5,41 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Log4Debug;
+using System.Diagnostics;
+using Log4Debug.Properties;
 
 namespace Log4Debug
 {
     public partial class Main : Form
     {
+        private LogAnalyser m_logAnalyser = null;
+
         public Main()
         {
             InitializeComponent();
+
+            connectToolStripMenuItem.Text = Resources.Connect;
+        }
+
+        private void CloseWorkers()
+        {
+            if (connectToolStripMenuItem.Text.Equals(Resources.CutConnect))
+            {
+                timer.Stop();
+
+
+                Debug.Assert(null != m_logAnalyser);
+                m_logAnalyser.Stop();
+                m_logAnalyser = null;
+
+            }
+
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            CloseWorkers();
             Close();
         }
 
@@ -30,6 +53,77 @@ namespace Log4Debug
         private void errorToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            List<string> logList = null;
+            List<LogLevel> logLevelList = null;
+
+            m_logAnalyser.GetLogs(out logList, out logLevelList);
+            if (null != logList)
+            {
+                for (int i = 0; i < logList.Count; ++i)
+                {
+                    switch(logLevelList[i])
+                    {
+                        case LogLevel.Debug:
+                            richTextBox.SelectionColor = Color.Green;
+                            break;
+                        case LogLevel.Info:
+                            richTextBox.SelectionColor = Color.White;
+                            break;
+                        case LogLevel.Warn:
+                            richTextBox.SelectionColor = Color.Yellow;
+                            break;
+                        case LogLevel.Error:
+                            richTextBox.SelectionColor = Color.Red;
+                            break;
+                        case LogLevel.Undefine:
+                            richTextBox.SelectionColor = Color.Gray;
+                            break;
+                        default:
+                            Debug.Assert(false);
+                            break;
+                    }
+
+                    richTextBox.AppendText(logList[i]);
+                }
+
+                logLevelList = null;
+                logList = null;
+            }
+        }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connectToolStripMenuItem.Text.Equals(Resources.Connect))
+            {
+                Debug.Assert(null == m_logAnalyser);
+                m_logAnalyser = new LogAnalyser();
+                m_logAnalyser.Start();
+
+                timer.Start();
+
+                connectToolStripMenuItem.Text = Resources.CutConnect;
+            }
+            else if (connectToolStripMenuItem.Text.Equals(Resources.CutConnect))
+            {
+                CloseWorkers();
+                
+
+                connectToolStripMenuItem.Text = Resources.Connect;
+            }
+            else
+            {
+                Debug.Assert(false);
+            }
+
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseWorkers();
         }
     }
 }
